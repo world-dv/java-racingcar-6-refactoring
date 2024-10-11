f11be818dc173515502394b0f7676d8f5f7f5e99
world-dv <goodforx0147@naver.com>
2024-10-12 00:59:23 +0900

    fix: print paramter type - change string to print message dto

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -2,23 +2,23 @@ package racingcar.view;
 
 public class OutputView {
 
-    public static void print(String message) {
-        System.out.println(message);
+    public static void print(PrintMessage printMessage) {
+        System.out.println(printMessage.getMessage());
     }
 
     public static void printCarNameInput() {
-        print(PrintMessage.NAME_INPUT_MESSAGE.getMessage());
+        print(PrintMessage.NAME_INPUT_MESSAGE);
     }
 
     public static void printCarMoveCount() {
-        print(PrintMessage.TRY_INPUT_MESSAGE.getMessage());
+        print(PrintMessage.TRY_INPUT_MESSAGE);
     }
 
     public static void printResult() {
-        print(PrintMessage.RESULT_MESSAGE.getMessage());
+        print(PrintMessage.RESULT_MESSAGE);
     }
 
     public static void printWinner() {
-        print(PrintMessage.WINNER_MESSAGE.getMessage());
+        print(PrintMessage.WINNER_MESSAGE);
     }
 }

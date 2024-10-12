3061f9647e49bca9a9d5b1bd3ec9bfdda63a3c36
world-dv <goodforx0147@naver.com>
2024-10-12 13:26:07 +0900

    feat: create updating advance

--- src/main/java/racingcar/domain/Car.java
+++ src/main/java/racingcar/domain/Car.java
@@ -7,12 +7,12 @@ public class Car {
     private final String carName;
     private Long advance;
 
-    public Car(String carName, Long advance) {
+    public Car(String carName) {
         if (checkCarName5Limit(carName)) {
             throw new IllegalArgumentException(ExceptionMessage.CAR_NAME_EXCEPTION.getMessage());
         }
         this.carName = carName;
-        this.advance = advance;
+        this.advance = 0L;
     }
 
     public boolean checkCarName5Limit(String carName) {

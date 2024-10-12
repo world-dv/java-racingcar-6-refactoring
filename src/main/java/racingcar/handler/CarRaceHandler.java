c44fc403463ebeea6a5feaa0005f48b248ae5b89
world-dv <goodforx0147@naver.com>
2024-10-13 02:11:58 +0900

    feat: handle car race result

---
+++ src/main/java/racingcar/handler/CarRaceHandler.java
@@ -0,0 +1,67 @@
+package racingcar.handler;
+
+import racingcar.util.MoveCar;
+
+import java.util.LinkedHashSet;
+
+public class CarRaceHandler {
+
+    private final LinkedHashSet<String> winners;
+    private final LinkedHashSet<MoveCar> moveCars;
+    private Integer winnerAdvance;
+
+    public CarRaceHandler(LinkedHashSet<MoveCar> moveCars) {
+        this.winners = new LinkedHashSet<>();
+        this.moveCars = moveCars;
+        this.winnerAdvance = 0;
+    }
+
+    public void makeMoveCar(Integer move) {
+        for (int moveCount = 0; moveCount < move; moveCount++) {
+            moveCars.forEach(MoveCar::move);
+            OutputHandler.printLineSpace();
+        }
+    }
+
+    public LinkedHashSet<String> findMaxAdvanceCarName() {
+        for (MoveCar car : moveCars) {
+            updateWinnerAdvance(car);
+            updateEqualWinnerAdvance(car);
+        }
+        return winners;
+    }
+
+    public void updateEqualWinnerAdvance(MoveCar car) {
+        if (compareEqualAdvance(car)) {
+            addWinner(car);
+        }
+    }
+
+    public void updateWinnerAdvance(MoveCar car) {
+        if (compareAdvance(car)) {
+            setWinnerAdvance(car);
+            removeAllWinner();
+            addWinner(car);
+        }
+    }
+
+    public boolean compareEqualAdvance(MoveCar car) {
+        return winnerAdvance.equals(car.getCarAdvance());
+    }
+
+    public boolean compareAdvance(MoveCar car) {
+        return winnerAdvance < car.getCarAdvance();
+    }
+
+    public void setWinnerAdvance(MoveCar car) {
+        winnerAdvance = car.getCarAdvance();
+    }
+
+    public void removeAllWinner() {
+        winners.clear();
+    }
+
+    public void addWinner(MoveCar car) {
+        winners.add(car.getCarName());
+    }
+}

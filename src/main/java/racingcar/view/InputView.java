3fbf1f457284a54592fa288ab702d8eb9f7cf769
world-dv <goodforx0147@naver.com>
2024-10-13 00:00:58 +0900

    fix: move input handle method to input handler

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -2,16 +2,9 @@ package racingcar.view;
 
 import camp.nextstep.edu.missionutils.Console;
 
-import java.util.HashSet;
-import java.util.List;
-
 public class InputView {
 
     public static String input() {
         return Console.readLine();
-    }
-
-    public static HashSet<String> inputCars() {
-        return new HashSet<>(List.of(input().split(",")));
     }
 }

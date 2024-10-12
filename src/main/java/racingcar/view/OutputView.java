da1620c3cd2b4d24e7ef8942bc92e11c692237ba
world-dv <goodforx0147@naver.com>
2024-10-12 22:43:02 +0900

    feat: print one line method to print message

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -6,15 +6,15 @@ public class OutputView {
         System.out.println(message);
     }
 
-    public static void printOneLine(String message) {
-        System.out.print(message);
-    }
-
     public static void printSpace() {
         System.out.println();
     }
 
     public static void printMessage(PrintMessage printMessage) {
         System.out.println(printMessage.getMessage());
+    }
+
+    public static void printMessageOneLine(PrintMessage message) {
+        System.out.print(message.getMessage());
     }
 }

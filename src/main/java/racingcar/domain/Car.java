dd553898182bf9f6e694047de27ff5322d31ea56
world-dv <goodforx0147@naver.com>
2024-10-12 10:03:58 +0900

    feat: validate car name length is 1 - 5

--- src/main/java/racingcar/domain/Car.java
+++ src/main/java/racingcar/domain/Car.java
@@ -1,13 +1,22 @@
 package racingcar.domain;
 
+import racingcar.exception.ExceptionMessage;
+
 public class Car {
 
     private final String carName;
     private final Long tryCount;
 
     public Car(String carName, Long tryCount) {
+        if (checkCarName5Limit()) {
+            throw new IllegalArgumentException(ExceptionMessage.CAR_NAME_EXCEPTION.getMessage());
+        }
         this.carName = carName;
         this.tryCount = tryCount;
+    }
+
+    public boolean checkCarName5Limit() {
+        return carName == null || carName.length() > 5;
     }
 
     public String getCarName() {

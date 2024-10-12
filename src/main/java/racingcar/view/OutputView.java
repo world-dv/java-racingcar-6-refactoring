8cd5010edb1ff65fbb99187f832c5fd259911e5c
world-dv <goodforx0147@naver.com>
2024-10-12 21:20:23 +0900

    fix: separate print message method

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -10,19 +10,4 @@ public class OutputView {
         System.out.println(printMessage.getMessage());
     }
 
-    public static void printCarNameInput() {
-        printMessage(PrintMessage.NAME_INPUT_MESSAGE);
-    }
-
-    public static void printCarMoveCount() {
-        printMessage(PrintMessage.TRY_INPUT_MESSAGE);
-    }
-
-    public static void printResult() {
-        printMessage(PrintMessage.RESULT_MESSAGE);
-    }
-
-    public static void printWinner() {
-        printMessage(PrintMessage.WINNER_MESSAGE);
-    }
 }

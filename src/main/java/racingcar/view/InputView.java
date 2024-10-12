eb16b366dbcffc0c7c7bc8245f04dde10d3fb7ea
world-dv <goodforx0147@naver.com>
2024-10-12 18:27:50 +0900

    feat: split ',' to make cars in input car names

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -1,10 +1,19 @@
 package racingcar.view;
 
 import camp.nextstep.edu.missionutils.Console;
+import racingcar.domain.Car;
+import racingcar.util.MoveCar;
+
+import java.util.HashSet;
+import java.util.List;
 
 public class InputView {
 
     public static String input() {
         return Console.readLine();
+    }
+
+    public static HashSet<String> inputCars() {
+        return new HashSet<>(List.of(input().split(",")));
     }
 }

942be69473e3500e8ae7ec98932bac039eb78782
world-dv <goodforx0147@naver.com>
2024-10-12 10:36:46 +0900

    fix: check input car name not saved car name

--- src/main/java/racingcar/domain/Car.java
+++ src/main/java/racingcar/domain/Car.java
@@ -8,14 +8,14 @@ public class Car {
     private final Long advance;
 
     public Car(String carName, Long advance) {
-        if (checkCarName5Limit()) {
+        if (checkCarName5Limit(carName)) {
             throw new IllegalArgumentException(ExceptionMessage.CAR_NAME_EXCEPTION.getMessage());
         }
         this.carName = carName;
         this.advance = advance;
     }
 
-    public boolean checkCarName5Limit() {
+    public boolean checkCarName5Limit(String carName) {
         return carName == null || carName.length() > 5;
     }
 

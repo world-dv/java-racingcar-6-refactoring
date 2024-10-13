8efa323cc2022e89a84de37c8c462601079371cd
world-dv <goodforx0147@naver.com>
2024-10-14 00:03:46 +0900

    feat: create car movement try count exception

--- src/main/java/racingcar/exception/ExceptionMessage.java
+++ src/main/java/racingcar/exception/ExceptionMessage.java
@@ -3,7 +3,8 @@ package racingcar.exception;
 public enum ExceptionMessage {
 
     CAR_INPUT_EXCEPTION("자동차 이름은 ','로 구분해야 합니다."),
-    CAR_NAME_EXCEPTION("자동차 이름은 5자리 이하만 가능합니다.");
+    CAR_NAME_EXCEPTION("자동차 이름은 5자리 이하만 가능합니다."),
+    CAR_MOVE_EXCEPTION("자동차 이동 시도 횟수는 정수만 가능합니다.");
 
     private final String message;
 
